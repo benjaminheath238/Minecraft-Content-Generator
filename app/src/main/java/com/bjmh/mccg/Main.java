ad0d99696f8851746820e3d24349262778c09845
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-18 11:25:25 +0100

    Basic setup

--- app/src/main/java/com/bjmh/mccg/Main.java
+++ app/src/main/java/com/bjmh/mccg/Main.java
@@ -1,11 +1,7 @@
 package com.bjmh.mccg;
 
 public class Main {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new Main().getGreeting());
+
     }
 }
--- app/src/test/java/com/bjmh/mccg/MainTest.java
+++ app/src/test/java/com/bjmh/mccg/MainTest.java
@@ -1,11 +1,7 @@
 package com.bjmh.mccg;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-    @Test void appHasAGreeting() {
-        Main classUnderTest = new Main();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+    @Test void appHasAGreeting() {}
 }
