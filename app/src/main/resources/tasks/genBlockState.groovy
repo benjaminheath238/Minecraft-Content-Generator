d8fc7e31b30fb66613bd646fa4da3a6e90b66d2a
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-19 16:35:08 +0100

    Added all default generators

--- app/src/main/resources/tasks/genBlockState.groovy
+++ app/src/main/resources/tasks/genBlockState.groovy
@@ -1,16 +1,9 @@
-// Stop if the section is not a block
-if (!section.getChildValue("type").equals("block")) return;
+if (section.getChildValue("layer").equals("true") || !section.getChildValue("type").equals("block")) return;
 
 new File("${pathBlockStates}${section.getChildValue("path")}").mkdirs();
 
-// Load the block state template
-println "Loading json."
 def template = loadJson("${dir}/templates/templateBlockState.json");
 
-// Update variables in the template
-println "Updating json."
 updateJsonVariables(template, variables)
 
-// Save the modified template
-println "Saving json."
 saveJson(template, "${pathBlockStates}${section.getChildValue("path")}${section.getName()}.json")
--- app/src/main/resources/tasks/genModel.groovy
+++ app/src/main/resources/tasks/genModel.groovy
@@ -1,53 +1,38 @@
+if (section.getChildValue("layer").equals("true")) return;
+
 if (section.getChildValue("type").equals("block")) {
     genBlockModel()
     genBlockItemModel()
 } else if (section.getChildValue("type").equals("item")) {
     genItemModel()
-} else {
-    println "This type does not have a model."
 }
 
 void genBlockModel() {
-    new File("${pathModelsBlock}${section.getChildValue("path")}").mkdirs();
-    // Load the block state template
-    println "Loading json."
+    new File("${pathModelsBlock}${section.getChildValue("path")}").mkdirs()
+    
     def template = loadJson("${dir}/templates/templateBlockModel.json")
 
-    // Update variables in the template
-    println "Updating json."
     updateJsonVariables(template, variables)
 
-    // Save the modified template
-    println "Saving json."
     saveJson(template, "${pathModelsBlock}${section.getChildValue("path")}${section.getName()}.json")
 }
 
 void genBlockItemModel() {
-    new File("${pathModelsItem}${section.getChildValue("path")}").mkdirs();
-    // Load the block state template
-    println "Loading json."
+    new File("${pathModelsItem}${section.getChildValue("path")}").mkdirs()
+    
     def template = loadJson("${dir}/templates/templateBlockItemModel.json")
 
-    // Update variables in the template
-    println "Updating json."
     updateJsonVariables(template, variables)
 
-    // Save the modified template
-    println "Saving json."
     saveJson(template, "${pathModelsItem}${section.getChildValue("path")}${section.getName()}.json")
 }
 
 void genItemModel() {
-    new File("${pathModelsItem}${section.getChildValue("path")}").mkdirs();
-    // Load the block state template
-    println "Loading json."
+    new File("${pathModelsItem}${section.getChildValue("path")}").mkdirs()
+    
     def template = loadJson("${dir}/templates/templateItemModel.json")
 
-    // Update variables in the template
-    println "Updating json."
     updateJsonVariables(template, variables)
 
-    // Save the modified template
-    println "Saving json."
     saveJson(template, "${pathModelsItem}${section.getChildValue("path")}${section.getName()}.json")
 }
