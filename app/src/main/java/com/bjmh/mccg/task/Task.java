43fd005604e1a8faf16f456f260405dfbcbb2798
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-19 11:39:56 +0100

    Added path variables to tasks

--- app/src/main/java/com/bjmh/mccg/task/Task.java
+++ app/src/main/java/com/bjmh/mccg/task/Task.java
@@ -7,7 +7,14 @@ import com.bjmh.mccg.Main;
 import groovy.lang.Script;
 
 public abstract class Task extends Script {
-    protected final String modid = ((ConfigOption) Main.GLOBAL_CONFIG.getChild(Main.MODID_KEY)).getValue();
+    protected final String modid = ((ConfigOption) Main.GLOBAL_CONFIG.getChild("modid")).getValue();
+
+    protected final String blockStatesPath = Main.USER_DIR + "assets/blockstates/";
+    protected final String blockModelsPath = Main.USER_DIR + "assets/models/block/";
+    protected final String itemModelsPath = Main.USER_DIR + "assets/models/item/";
+    protected final String blockTexturesPath = Main.USER_DIR + "assets/textures/block/";
+    protected final String itemTexturesPath = Main.USER_DIR + "assets/texutes/item/";
+    protected final String langPath = Main.USER_DIR + "assets/lang/";
 
     protected ConfigSection section;
 
