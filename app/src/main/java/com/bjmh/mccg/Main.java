88c97ea27104dc64648914ff9d05e1bf6c3a687b
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-19 15:41:03 +0100

    Readded prints

--- app/src/main/java/com/bjmh/mccg/Main.java
+++ app/src/main/java/com/bjmh/mccg/Main.java
@@ -74,6 +74,9 @@ public class Main {
                         e.printStackTrace();
                     }
 
+                    System.err.println("Running task " + task);
+                    System.out.println("Running task " + task);
+
                     TASKS.get(task).run(section);
                 }
             }
