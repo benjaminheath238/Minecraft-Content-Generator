0ccb9ed32e851c1126f470039281b15e76d4f1df
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-21 09:43:15 +0100

    Added missing quotes

--- app/src/main/resources/tasks/genLocale.groovy
+++ app/src/main/resources/tasks/genLocale.groovy
@@ -7,7 +7,7 @@ String name = (section.getChild('name') == null)
         : section.getChildValue('name')
 
 // Append this sections entry in the format "TYPE.MODID.ID": "NAME"
-appendToJson("${pathLang}/en_us.json", "${section.getChildValue('type')}.${modid}.${section.getChildValue('id')}: ${name}")
+appendToJson("${pathLang}/en_us.json", "    \"${section.getChildValue('type')}.${modid}.${section.getChildValue('id')}\": \"${name}\"")
 
 // Converts an id to a name
 // by replacing "_" with white space and capitalizing the next letter
